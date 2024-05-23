40aaa5a935f143f429de818bace06a8e971409d6
Murat Oztemel <cegerxwin@gmail.com>
2024-05-23 23:37:45 +0300

    Project Starts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,16 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image, useColorMode } from "@chakra-ui/react";
 
 const Header = () => {
-  return <Flex>Header</Flex>;
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Flex justifyContent={"center"} mt={6} mb={12}>
+      <Image
+        cursor={"pointer"}
+        alt="logo"
+        w={6}
+        src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+      />
+    </Flex>
+  );
 };
 export default Header;
